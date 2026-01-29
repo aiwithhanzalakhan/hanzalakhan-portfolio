2076ddde2451a3c4817636fd104ff88fc74e18cb
Hanzalakhan6226 <ahadk1364@gmail.com>
2026-01-30 00:56:34 +0500

    com

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,32 @@
-import type { NextAuthOptions } from "next-auth";
+// import type { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         username: { label: "Username", type: "text" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (
+//           credentials?.username === process.env.ADMIN_USER &&
+//           credentials?.password === process.env.ADMIN_PASS
+//         ) {
+//           return { name: credentials.username };
+//         }
+//         return null;
+//       },
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
 import CredentialsProvider from "next-auth/providers/credentials";
+import type { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -10,12 +37,21 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if (
-          credentials?.username === process.env.ADMIN_USER &&
-          credentials?.password === process.env.ADMIN_PASS
-        ) {
-          return { name: credentials.username };
+        
+        if (!credentials?.username || !credentials?.password) {
+          return null;
         }
+
+        if (
+          credentials.username === process.env.ADMIN_USER &&
+          credentials.password === process.env.ADMIN_PASS
+        ) {
+          return {
+            id: "admin",        
+            name: credentials.username,
+          };;
+        }
+
         return null;
       },
     }),
