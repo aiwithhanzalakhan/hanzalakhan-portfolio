27a602711b1ec03db906d00abbe932c29153966d
Hanzalakhan6226 <ahadk1364@gmail.com>
2026-01-30 00:08:14 +0500

    ch

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,9 @@
 // export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
-        },
-      ],
-      eslint: {
-        ignoreDuringBuilds: true, // optional
-      },
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;  
